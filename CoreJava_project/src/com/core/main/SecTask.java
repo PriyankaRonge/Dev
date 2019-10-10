43dbf5c57bfb4325ae620520358b2ce9541342f6
Priyanka Ronge <rongepriyanka11@gmail.com>
2019-10-10 14:52:49 +0530

    sixth commit- added reverted changes to sectask

--- CoreJava_project/src/com/core/main/SecTask.java
+++ CoreJava_project/src/com/core/main/SecTask.java
@@ -3,8 +3,10 @@ package com.core.main;
 public class SecTask {
 public static void main(String[] args) {
 	System.out.println("second task.....");
+
 	int d =20+30;
 	System.out.println("sum : "+d);
 	System.out.println();
+
 }
 }
