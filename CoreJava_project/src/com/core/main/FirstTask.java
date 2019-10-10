1136b2e98a3647993d8c262c899473438a6e5a0b
Priyanka Ronge <rongepriyanka11@gmail.com>
2019-10-10 14:31:28 +0530

    second commit

--- CoreJava_project/src/com/core/main/FirstTask.java
+++ CoreJava_project/src/com/core/main/FirstTask.java
@@ -3,5 +3,9 @@ package com.core.main;
 public class FirstTask {
 	 public static void main(String[] args) {
 		System.out.println("first print message...");
+		
+		int c=10+20;
+		
+		System.out.println(" sum : "+c);
 	}
 }
