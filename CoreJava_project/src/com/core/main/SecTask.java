60d9cd485370fd362172771253f8a3396b2237fb
Priyanka Ronge <rongepriyanka11@gmail.com>
2019-10-11 13:42:40 +0530

    This is first commit after creation of second  branch'

--- CoreJava_project/src/com/core/main/SecTask.java
+++ CoreJava_project/src/com/core/main/SecTask.java
@@ -8,5 +8,8 @@ public static void main(String[] args) {
 	System.out.println("sum : "+d);
 	int a= 90-45;
 	System.out.println("sub : "+a);
+	int b= 10*8;
+	System.out.println("multiplication : "+b);
+	
 }
 }
