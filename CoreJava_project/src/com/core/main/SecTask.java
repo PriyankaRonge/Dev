16931f0e9c3b4a65f3e77312b2f7efab45cbb21e
PriyankaRonge <rongepriyanka11@gmail.com>
2019-10-11 15:55:35 +0530

    Checking git pull command

--- CoreJava_project/src/com/core/main/SecTask.java
+++ CoreJava_project/src/com/core/main/SecTask.java
@@ -14,6 +14,8 @@ public static void main(String[] args) {
 	int t= 10/10;
 	System.out.println("Division : "+t);
 	
-	System.out.println("This is all about operations ........"); 
+	System.out.println("This is all about operations ........");
+	System.out.println("changes pushd to github successfully...");
+	
 }
 }
